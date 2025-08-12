f4b03b59f9a3871a8e2cda55f1213a25c2db5691
KAIQUE ONENCIO DE SOUZA <0201392321040>
2025-08-12 12:55:21 -0300

    Aula 01 - 12/08

---
+++ Projeto1DW/Projeto1DW/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto1DW.Models;
+
+namespace Projeto1DW.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Produto> Produtos {  get; set; } // criando tabela de Produtos
+    }
+}
---
+++ Projeto1DW/Projeto1DW/Models/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace Projeto1DW.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }   // pode ser NULO, pois é nullable
+        public double Preco { get; set; }
+    }
+}
